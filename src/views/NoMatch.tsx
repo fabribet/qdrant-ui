3ba0a83ee7516382ae5eb77559a8a2bc402ffd10
Fabrizio Bettinelli <fabribettinelli@gmail.com>
2023-03-15 15:29:40 -0300

    Improve no-match view

--- src/views/NoMatch.tsx
+++ src/views/NoMatch.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function NoMatch() {
   return (
     <div>
-      {"This route doesn't exist. "}
+      {"This page doesn't exist. "}
       <Link to="/">Go to the main page</Link>
     </div>
   );
